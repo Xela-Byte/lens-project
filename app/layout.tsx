6491ead7f4736c1c9814270da866cbb7b119e9a4
Xela-Byte <xelaoladipupo@gmail.com>
2023-09-08 22:35:38 +0100

    Fixed SEO errors, for title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,11 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { fetchGlobalData } from "@/stateManagement/actions/fetchGlobalData";
 import { Providers } from "@/stateManagement/provider";
-import {
-  AppDispatch,
-  AppThunkDispatch,
-  persistor,
-  store,
-} from "@/stateManagement/store";
+import { persistor, store } from "@/stateManagement/store";
 import { Nunito_Sans } from "next/font/google";
 import { useEffect } from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import "./globals.css";
-import { useDispatch } from "react-redux";
-import Head from "next/head";
 
 const nunito = Nunito_Sans({ subsets: ["latin"] });
 
@@ -30,11 +23,6 @@ export default function RootLayout({
 
   return (
     <html>
-      <Head>
-        <title>Wallet Street Lens</title>
-        <meta property="og:title" content="Wallet Street Lens" key="title" />
-        <meta name="description" content="Wallet Lens Street, stock" />
-      </Head>
       <body className={nunito.className}>
         <Providers>
           <PersistGate loading={null} persistor={persistor}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Home from "@/components/Home";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Wallet Street Lens",
+  description: "Wallet Street Lens, stock",
+};
+
 type Props = {};
 
 const App = ({}: Props) => {
